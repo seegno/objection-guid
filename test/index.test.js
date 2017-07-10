c5dc4bc88b4f6949431da82d4ad98dd48d1b0474
João Gonçalves <joaogoncalves@seegno.com>
2017-07-10 15:14:08 +0100

    Add field and generateGuid plugin options

--- test/index.test.js
+++ test/index.test.js
@@ -1,28 +1,60 @@
 'use strict';
 
 /**
+ * Jest mocks.
+ */
+
+jest.mock('uuid', () => ({ v4: jest.fn(() => 'foobar') }));
+
+/**
  * Module dependencies.
  */
 
-const TestModel = require('./fixtures/test-model');
+const modelFactory = require('./utils/model-factory');
 
 /**
  * Test `guid` plugin.
  */
 
 describe('GuidIdPlugin', () => {
-  let model;
-
-  beforeEach(() => {
-    model = new TestModel();
-  });
-
   describe('$beforeInsert', () => {
-    it('should add `id` properties to the model', () => {
+    it('should add the `id` property to the model with the default value of generated guid', () => {
+      const model = modelFactory();
       const parent = model.$beforeInsert();
 
       return Promise.resolve(parent).then(() => {
-        expect(model).toEqual({ id: expect.any(String) });
+        expect(model).toEqual({ id: 'foobar' });
+      });
+    });
+
+    it('should add the property provided as an option to the model', () => {
+      const model = modelFactory({ field: 'foo' });
+      const parent = model.$beforeInsert();
+
+      return Promise.resolve(parent).then(() => {
+        expect(model).toEqual({ foo: expect.any(String) });
+      });
+    });
+
+    it('should add the `id` property to the model and set the value as the result of the `generateGuid` function provided as an option', () => {
+      const model = modelFactory({ generateGuid: () => 'foo' });
+      const parent = model.$beforeInsert();
+
+      return Promise.resolve(parent).then(() => {
+        expect(model).toEqual({ id: 'foo' });
+      });
+    });
+
+    it('should add the property provided as option to the model and set the value as the result of the `generateGuid` function provided as an option', () => {
+      const model = modelFactory({
+        field: 'foo',
+        generateGuid: () => new Promise(resolve => resolve('bar'))
+      });
+
+      const parent = model.$beforeInsert();
+
+      return Promise.resolve(parent).then(() => {
+        expect(model).toEqual({ foo: 'bar' });
       });
     });
   });
