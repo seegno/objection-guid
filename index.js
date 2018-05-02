3bb996f435e4df3463a97c01eedaa05d68a727bb
Nuno Rafael Rocha <nunorafaelrocha@gmail.com>
2018-05-02 12:15:00 +0100

    Update source code to follow prettier formatter

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const uuid = require('uuid');
  */
 
 module.exports = options => {
-  options = Object.assign({
-    field: 'id',
-    generateGuid: () => uuid.v4()
-  }, options);
+  options = Object.assign(
+    {
+      field: 'id',
+      generateGuid: () => uuid.v4()
+    },
+    options
+  );
 
   return Model => {
     return class extends Model {
-
       /**
        * Before insert.
        */
@@ -27,7 +29,10 @@ module.exports = options => {
         const parent = super.$beforeInsert(context);
 
         return Promise.resolve(parent)
-          .then(() => this[options.field] || options.generateGuid.call(this, context))
+          .then(
+            () =>
+              this[options.field] || options.generateGuid.call(this, context)
+          )
           .then(guid => {
             this[options.field] = guid;
           });
